bdc82b0c04c97c942e33e271b5c9f41c28c4d541
davidmp481 <davidmpeterson1998@gmail.com>
2025-05-07 13:30:14 -0500

    Delete main.jsx

--- main.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import FlightSearch from './App';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<FlightSearch />);
